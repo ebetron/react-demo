f43c987e1dbbf8354e73f309b518067d165f61ac
ebetron <ebetron@gmail.com>
2018-05-15 18:59:02 -0400

    Pull state up from Board into Game

--- src/index.js
+++ src/index.js
@@ -11,14 +11,6 @@ function Square(props){
 }
 
 class Board extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
-
   handleClick(i){
     const squares = this.state.squares.slice();
     if(calculateWinner(squares) || squares[i]){
@@ -32,7 +24,7 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={()=> this.handleClick(i)} />;
+  return (<Square value={this.props.squares[i]} onClick={()=> this.props.onClick(i)} />);
   }
 
   render() {
@@ -68,6 +60,15 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
   render() {
     return (
       <div className="game">
@@ -102,7 +103,7 @@ function calculateWinner(squares){
     [2,4,6],
   ];
   for (let i = 0; i < lines.length; i++){
-    const [a,b,c] = lines [i];
+    const [a,b,c] = lines[i];
     if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
       return squares[a];
     }
